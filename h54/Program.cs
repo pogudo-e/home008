94acfb4b7a7123d6ab8d7be11f7fc72237711fb1
Evgeny Pogudo <sensesbit@gmail.com>
2022-08-15 23:29:45 +0300

    inital commit

---
+++ h54/Program.cs
@@ -0,0 +1,52 @@
+﻿int m = 4;
+int n = 3;
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(10);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ h56/Program.cs
@@ -0,0 +1,31 @@
+﻿int m = 4;
+int n = 4;
+
+int[,] array = new int[m,n];
+
+for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+        array[i,j] = new Random().Next(0, 10);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine("");
+}
+
+void Por(int[,] array){
+    int retur = 40;
+    int str = 0;
+    for(int i = 0; i < m; i++){
+       int ans = 0; 
+        for(int j = 0; j < n; j++){
+            ans += array[i,j];
+        }
+        if (ans < retur){
+           retur = ans;
+           str = i+1;
+        }
+    }
+    Console.WriteLine($"{str} строка имеет наименьшую сумму элементов");
+
+}
+
+Por(array);
